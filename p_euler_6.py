1add006aea949f58d4ee45698b27f2c3b9e33636
adamwellsward <warda5@byu.edu>
2024-04-16 21:42:47 -0600

    Solution

--- p_euler_6.py
+++ p_euler_6.py
@@ -4,5 +4,12 @@ Project Euler Problem 6 Solution
 Adam Ward
 """
 
+import numpy as np
+
+def sum_square_diff(N):
+    nums = np.arange(1,N+1)
+    return (np.sum(nums))**2 - np.sum(nums**2)
+
 if __name__ == "__main__":
+    print(sum_square_diff(100))
     pass
