563f658169b23e03f4950fbbb02a8e0fcff00501
adamwellsward <warda5@byu.edu>
2024-04-16 22:54:34 -0600

    Solution

--- p_euler_9.py
+++ p_euler_9.py
@@ -4,5 +4,18 @@ Project Euler Problem 9 Solution
 Adam Ward
 """
 
+import numpy as np
+
+def pyth_triplet():
+    for a in range(1000):
+        for b in range(a,1000):
+            c = 1000 - b - a
+            if c <= b:
+                break
+            if a**2 + b**2 == c**2:
+                return a*b*c
+    return -1
+
 if __name__ == "__main__":
+    # print(pyth_triplet())
     pass
