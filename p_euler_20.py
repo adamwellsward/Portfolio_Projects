6ddfe82ff6ea6c898524a7cb44447b484c8e29ea
adamwellsward <warda5@byu.edu>
2024-05-09 14:59:09 -0600

    Solution

--- p_euler_20.py
+++ p_euler_20.py
@@ -4,5 +4,14 @@ Project Euler Problem 20 Solution
 Adam Ward
 """
 
+import numpy as np
+from math import factorial
+
+def factorial_sum():
+    num = factorial(100)
+    digits = np.array(list(str(num))).astype(int)
+    return np.sum(digits)
+
 if __name__ == "__main__":
+    print(factorial_sum())
     pass
