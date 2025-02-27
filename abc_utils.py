a751644e4b34f7774522a7dd8c209a7c6df569c4
Matthew Ward <matthew.merrill.ward@gmail.com>
2025-02-27 12:58:42 -0700

    Better states, still need to work on key changes

--- abc_utils.py
+++ abc_utils.py
@@ -17,10 +17,6 @@ def abc_to_dataframe(abc_text: str,
     states_keys = ['measure', 'beat', 'chord', 'melody']
     states = {key: [] for key in states_keys}
 
-    current_key = None
-    current_melody = None
-    current_chord = None
-
     def _append_line(
             measure: int, 
             beat: float, 
@@ -29,16 +25,13 @@ def abc_to_dataframe(abc_text: str,
             key: m21.key.Key,
             include_mode: bool = False
         ):
-        new_measure = measure
-        new_beat = beat
-        new_chord = chord.romanNumeral.romanNumeral if chord else '-'
-        
+        chord_repr = chord.romanNumeral.romanNumeral if chord else '-'
         
         # If rest, use an hyphen
         if not melody:
-            new_melody = '-'
+            melody_repr = '-'
         elif isinstance(melody, m21.note.Rest):
-            new_melody = '-'
+            melody_repr = '-'
         elif isinstance(melody, m21.note.Note):
             # TODO: add more modes
             mode_shift = 0  
@@ -51,23 +44,62 @@ def abc_to_dataframe(abc_text: str,
                 else:
                     raise Exception(f'Mode is {mode}. Not major or minor')
             key_shift = (key.tonic.midi % 12) + mode_shift
-            new_melody = melody.pitch.midi - key_shift
+            melody_repr = melody.pitch.midi - key_shift
         else:
             raise Exception('Melody should be a rest or a note')
-        
-        # Check if this should even be a new line
-        if len(states['melody']) > 0 and new_chord == states['chord'][-1] and new_melody == states['melody'][-1]:
-            return
             
-        states['measure'].append(new_measure)
-        states['beat'].append(new_beat)
-        states['chord'].append(new_chord)
-        states['melody'].append(new_melody)
+        states['measure'].append(measure)
+        states['beat'].append(beat)
+        states['chord'].append(chord_repr)
+        states['melody'].append(melody_repr)
+
+    current_key = None
+    current_melody = None
+    current_chord = None
+    current_measure = None
+    current_beat = None
     
+    def not_meaningful(element):
+        return isinstance(
+            element,
+            (
+                m21.spanner.RepeatBracket,
+                m21.bar.Repeat
+            )
+        )
+
+
     for index, element in enumerate(part.flatten()):
+        if not_meaningful(element): 
+            continue
+            
+        this_beat = element.beat
+        this_measure = element.measureNumber
+
+        # The first time an object has measure and beat, use that to initialize current_beat and current_measure
+        if current_beat is None and current_measure is None and this_beat is not None and this_measure is not None:
+            current_beat = this_beat
+            current_measure = this_measure
+
+        # If the time in the music has changed, append the old stuff
+        if (this_measure != current_measure or this_beat != current_beat) and (current_chord is not None or current_melody is not None): 
+            # Append the last new state
+            _append_line(
+                current_measure,
+                current_beat,
+                current_chord,
+                current_melody,
+                current_key
+            )
+            # Update current measure
+            current_measure = this_measure
+            current_beat = this_beat
+        
+        
         if isinstance(element, m21.key.Key):
             current_key = element
-        if isinstance(element, m21.harmony.ChordSymbol):
+
+        elif isinstance(element, m21.harmony.ChordSymbol):
             # Make sure the chord is relative to the current key
             if current_key is None:
                 raise Exception('Current key unknown')
@@ -75,67 +107,12 @@ def abc_to_dataframe(abc_text: str,
             element.key = current_key
             current_chord = element
             
-        if isinstance(element, m21.note.Note):
+        elif isinstance(element, m21.note.Note):
             if current_key is None:
                 raise Exception('Current key unknown')
             
             element.key = current_key
             current_melody = element
-        
-        # Append the new state
-        _append_line(
-            element.measureNumber,
-            element.beat,
-            current_chord,
-            current_melody,
-            current_key
-        )
-
-    
-    return pd.DataFrame(states)
-
-def abc_to_states(abc_text: str, chords_per_state: int):
-    abc_score = m21.converter.parse(abc_text, format='abc')
-    # The score should only have one part, which contains notes, chords, etc.
-    part = abc_score.parts[0]
-
-    # Flatten the part to iterate through everything in it.
-    # TODO: See also m21.Stream.notes, which is an iterator only containing notes, chords, etc. No rests.
-    # TODO: It looks like there's also a way to get timestamps in seconds. See Stream.seconds
-    # TODO: use element.quarterLength to "standardize" meter
-    for element in part.flatten().notesAndRests:
-        if isinstance(element, m21.chord.Chord):
-            measure = element.measureNumber
-            beat = element.quarterLength
-            print(f"\nChord: {element}\n\tMeasure {measure}\n\tDuration {beat}")
-        elif isinstance(element, m21.note.Note):
-            measure = element.measureNumber
-            beat = element.quarterLength
-            print(f"\nNote:  {element}\n\tMeasure {measure}\n\tDuration {beat}")
-        elif isinstance(element, m21.note.Rest):
-            measure = element.measureNumber
-            beat = element.quarterLength
-            print(f"\nRest:  {element}\n\tMeasure {measure}\n\tDuration {beat}")
-
-    # Prepare to collect states
-    states = []
-    current_key = None
-
-    if chords_per_state != 1:
-        raise Exception('Not implemented')
-    for index, element in enumerate(part.flatten()):
-        if isinstance(element, m21.key.Key):
-            current_key = element
-        if isinstance(element, m21.harmony.ChordSymbol):
-            # Make sure the chord is relative to the current key
-            if current_key is None:
-                raise Exception('Current key unknown')
-            element.key = current_key
-            states.append(pd.Series({
-                'measure': element.measureNumber,
-                'beat': element.beat,
-                'chord': element.romanNumeral.romanNumeral
-            }))
     
     return pd.DataFrame(states)
 
@@ -159,6 +136,9 @@ def dataset_to_abc(dataset_abc_text: str, label, reference_number):
     if not re.search(r'^\s*T:', metadata):
         metadata = f'T:{label} {reference_number}\n' + metadata
     
+    # Fix key changes throughout tune
+    # TODO: add new lines as in 3.9 of   https://trillian.mit.edu/~jc/music/doc/ABC.html  
+
     fixed_text = metadata + '|' + tune
     return fixed_text
 
@@ -212,5 +192,6 @@ def load_harmonization_train_test(local=True):
 if __name__ == '__main__':
     train_set, _ = load_harmonization_train_test()
 
-    abc_texts = [train_set.sample(1)['output'].item() for _ in range(100)]
-    print(abc_to_dataframe(abc_texts[0]))
+    abc_text = train_set.iloc[6]['output']
+    df = abc_to_dataframe(abc_text)
+    print()
