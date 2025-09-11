7ce5524f471081051046110c002b1ae87aa2eb4a
adamwellsward <warda5@byu.edu>
2025-09-11 09:56:37 -0600

    start

---
+++ project_euler/p_euler_23.py
@@ -0,0 +1,15 @@
+# p_euler_23.py
+"""
+Project Euler Problem 23 Solution
+Adam Ward
+"""
+
+import numpy as np
+
+def get_divisors(x):
+    '''Return a NumPy array with all of the factors.'''
+    return np.div
+
+if __name__ == "__main__":
+    
+    pass
