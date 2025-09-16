24766037dcd11aab600f7255b98c315a171fe972
adamwellsward <warda5@byu.edu>
2025-09-16 11:08:59 -0600

    progress on 19

--- project_euler/p_euler_19.py
+++
@@ -1,8 +0,0 @@
-# p_euler_19.py
-"""
-Project Euler Problem 19 Solution
-Adam Ward
-"""
-
-if __name__ == "__main__":
-    pass
--- project_euler/p_euler_22.py
+++
@@ -1,8 +0,0 @@
-# p_euler_22.py
-"""
-Project Euler Problem 22 Solution
-Adam Ward
-"""
-
-if __name__ == "__main__":
-    pass
